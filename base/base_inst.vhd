7eac37cae49f2a799c8c4f6c13ff43c68a5f113b
kayvan-faghani <kayfaghani@gmail.com>
2026-01-19 16:06:32 +0000

    Initial Commit

---
+++ base/base_inst.vhd
@@ -0,0 +1,13 @@
+	component task1 is
+		port (
+			clk_clk                            : in  std_logic                    := 'X'; -- clk
+			led_pio_external_connection_export : out std_logic_vector(7 downto 0)         -- export
+		);
+	end component task1;
+
+	u0 : component task1
+		port map (
+			clk_clk                            => CONNECTED_TO_clk_clk,                            --                         clk.clk
+			led_pio_external_connection_export => CONNECTED_TO_led_pio_external_connection_export  -- led_pio_external_connection.export
+		);
+
